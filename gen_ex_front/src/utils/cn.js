c082ef44379bf9bbc2f2175ab88809adace6d75d
magistral123 <jordanmagistral@gmail.com>
2025-12-30 15:55:42 +0100

    Delete gen_ex_front directory
    Ce dossier ne servait a rien

--- gen_ex_front/src/utils/cn.js
+++
@@ -1,8 +0,0 @@
-/**
- * Utility function to merge and deduplicate Tailwind CSS classes
- * @param  {...string} classes - CSS class strings to merge
- * @returns {string} Merged class string
- */
-export function cn(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
