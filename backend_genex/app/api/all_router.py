6f861ac8e7319d83948256c0cb1dbca656f17e61
Magistral-dev <jordanmaistral@gmail.com>
2026-01-18 18:50:09 +0100

    Suppression des fichiers inutiles

--- backend_genex/app/api/all_router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-from api.routes import routes, document_upload
-
-router = APIRouter()
-
-router.include_router(routes.router, prefix="/genex", tags=["GenEx"])
-router.include_router(document_upload.router, prefix="/genex", tags=["GenEx"])
--- backend_genex/app/utils/auth.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, date, timedelta
-from typing import Annotated
-from fastapi import Depends, HTTPException, APIRouter #surligne en jaune
-from sqlalchemy.orm import Session #surligne en jaune
-from starlette import status #surligne en jaune
-from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm  #surligne en jaune
-from jose import JWTError, jwt
